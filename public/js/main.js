01108876bd3ffb2b412bf0c6b659773d9c956f14
Kristian <kristian.morin@gmail.com>
2018-12-31 17:25:36 +0000

    Date picker fixed to show range with start 5 day before now()

--- public/js/main.js
+++ public/js/main.js
@@ -219,6 +219,8 @@ $(function() {
   $('input[name="daterange"]').daterangepicker({
     opens: 'left',
     drops: 'down',
+    endDate: moment().startOf('hour'),
+    startDate: moment().startOf('hour').subtract(5, 'day'),
   }, function(start, end, label) {
     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     let from  = start.format('YYYY-MM-DD');
