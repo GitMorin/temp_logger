e1d1a98fba50dfa3151eb80c749d37482cf764a3
Kristian <kristian.morin@gmail.com>
2019-01-05 15:10:59 +0000

    Renaming and refatoring a many of the functions to make it easier to follow. Added to displat current temperature only for acitve fermenting beers

--- public/js/main.js
+++ public/js/main.js
@@ -58,7 +58,10 @@ var myLineChart = new Chart(ctx, {
 let temp_room =null;
 let temp_beer =null;
 let temp_outdoor =null;
-let beer_list = null;
+let current_temp;
+//let beerObject;
+let beer_list;
+let displaying_beer
 
 // function targetTemp(date, temp) {
 //   this.x = date;
@@ -75,35 +78,75 @@ let temp_target = [{
   y: 20
 }];
 
-// Find object in beer list with beer name of X. Returning object
-function beerinfo(beerName) {
-  let beerObj = beer_list.filter(function(beer){
-    return beer.name === beerName;
-  })[0];
-  getFermentationDates(beerObj);
-  updateBeerDisplayInformation(beerObj);
+// Populate beer list to dropdown
+fetch('/api/beerlist')
+.then(function(response){
+  return response.json();
+})
+.then(function(beerList) {
+  // set global beer_list variable
+  beer_list = beerList
+  latestBeer = getLatestStartedBeer(beerList);
+  // set latest beer to global
+  displaying_beer = latestBeer
+  updateBeerTitle(latestBeer);
+
+  fermentTo = getEndFermentationDate(latestBeer);
+  plotDataBetween(latestBeer.start_ferment, fermentTo);
+  
+  // set temp target for latest beer
+  temp_target[0].y = latestBeer.target_temp;
+  temp_target[1].y = latestBeer.target_temp;
+
+  // Append beer to dropdown list
+  beerList.forEach(beer => {
+    let beerList = document.getElementById("beer-list");
+    const beerElement = document.createElement('a');
+    beerElement.setAttribute("class", "dropdown-item");
+    beerElement.setAttribute("href", "#");
+    beerElement.textContent = beer.name;
+    beerElement.addEventListener('click', function(e){
+      let beer_clicked = e.target.innerText
+      // set global variable with the beer to display
+      displaying_beer = getBeerObject(beer_clicked);
+      updateBeerTitle(displaying_beer);
+      fermentTo = getEndFermentationDate(displaying_beer);
+      plotDataBetween(displaying_beer.start_ferment, fermentTo);
+    })
+    beerList.appendChild(beerElement);
+  });
+});
+
+function getLatestStartedBeer(beerList) {
+  latestBeer = beerList.reduce(function (r, a) {
+    return r.date > a.date ? r : a;
+  });
+  return latestBeer;
 };
 
-function updateBeerDisplayInformation(beer) {
+function getEndFermentationDate(beer){
+  if (beer.end_ferment == null) {
+    return moment().format('YYYY-MM-DD HH:mm');
+  } else {
+    return beer.end_ferment;
+  }
+};
+
+// Find object in beer list with beer name of X. Returning object
+function getBeerObject(beerName) {
+  let beer = beer_list.filter(function(beer){
+    return beer.name === beerName;
+  });
+  return beer[0];
+};
+
+function updateBeerTitle(beer) {
   let currentBeer = document.querySelector("#current-beer strong");
   if (beer.is_active === true) {
     currentBeer.textContent = ` ${beer.name} - Currently fermenting`;
   } else {
-    // currentBeer = document.querySelector("#current-beer");
-    // currentBeer = document.querySelector("#current-beer");
     currentBeer.textContent = ` ${beer.name} - Is complete`;
   }
-}
-
-// Check if beer is in active fermentation. If more than one beer is active return the latest one
-function findAndAddActiveBeer(beerlist) {
-  // May not need this anymore. Only need the last beer
-  orderedBeer = beerlist.sort(function(a,b){
-    return Date.parse(b.start_ferment) - Date.parse(a.start_ferment);
-  });
-  // Display the beer name - this function need to be smarter to state if the beer is active o not..
-  updateBeerDisplayInformation(orderedBeer[0]);
-  getFermentationDates(orderedBeer[0]);
 }
 
 // If beer does not have end fermentation date yet (due to active fermentation) 
@@ -117,52 +160,10 @@ function getFermentationDates(beer) {
   getDataBetween(beer.start_ferment, fermentTo);
   temp_target[0].y = beer.target_temp;
   temp_target[1].y = beer.target_temp;
+
 }
 
-
-// Populate beer list to dropdown
-fetch('/api/beerlist')
-.then(function(response){
-  return response.json();
-})
-.then(function(beerlist) {
-  findAndAddActiveBeer(beerlist);
-  beer_list = beerlist;
-  beerlist.forEach(beer => {
-    // Append beer to dropdown list
-    let beerList = document.getElementById("beer-list");
-    const beerElement = document.createElement('a');
-    beerElement.setAttribute("class", "dropdown-item");
-    beerElement.setAttribute("href", "#");
-    beerElement.textContent = beer.name;
-    beerElement.addEventListener('click', function(e){
-      let beer_clicked = e.target.innerText
-      beerinfo(beer_clicked);
-    })
-    beerList.appendChild(beerElement);
-  });
-});
-
-function drawTargetTemp(data) {
-  // Set Target temp object
-  temp_target[0].x = data[0].x;
-  temp_target[1].x = data[data.length -1].x;
-
-  targetDataset = {
-    fill: false,
-    label: 'Target Temp',
-    data: temp_target,
-    borderColor: 'rgba(255,0,0)',
-    backgroundColor: 'rgba(255,0,0,0)',
-    pointRadius: 0,
-    borderWidth: 1,
-    pointHitRadius: 0,
-  };
-  myLineChart.data.datasets.push(targetDataset);
-  myLineChart.update();
-}
-
-function getDataBetween(from, to){
+function plotDataBetween(from, to){
   myLineChart.data.datasets.pop(temp_room);
   myLineChart.data.datasets.pop(temp_beer);
   myLineChart.data.datasets.pop(temp_outdoor);
@@ -202,6 +203,16 @@ function getDataBetween(from, to){
       pointHitRadius: 7.5
     };
     
+    current_temp = data[data.length -1].y;
+    const currentTemp = document.getElementById("current-temp");
+    if (displaying_beer.is_active == true) {
+      currentTemp.textContent = current_temp;
+    } else {
+      currentTemp.textContent = '';
+    }
+    // set temp target
+    temp_target[0].x = data[0].x;
+    temp_target[1].x = data[data.length -1].x;
     drawTargetTemp(data);
     myLineChart.data.datasets.push(dataset);
     myLineChart.update();
@@ -227,6 +238,25 @@ function getDataBetween(from, to){
   });
 };
 
+function drawTargetTemp(data) {
+  // Set Target temp date
+  temp_target[0].x = data[0].x;
+  temp_target[1].x = data[data.length -1].x;
+
+  targetDataset = {
+    fill: false,
+    label: 'Target Temp',
+    data: temp_target,
+    borderColor: 'rgba(255,0,0)',
+    backgroundColor: 'rgba(255,0,0,0)',
+    pointRadius: 0,
+    borderWidth: 1,
+    pointHitRadius: 0,
+  };
+  myLineChart.data.datasets.push(targetDataset);
+  myLineChart.update();
+}
+
 $(function() {
   $('input[name="daterange"]').daterangepicker({
     opens: 'left',
@@ -237,35 +267,6 @@ $(function() {
     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     let from  = start.format('YYYY-MM-DD');
     let to = end.format('YYYY-MM-DD')
-    getDataBetween(from, to);
+    plotDataBetween(from, to);
   });
 });
-
-let exampleData = [{
-    x: '2018-12-06 03:00:00',
-    y: 19
-}, {
-    x: '2018-12-06 10:00:00',
-    y: 40
-}, {
-    x: '2018-12-06 23:00:00',
-    y: 0
-},{
-    x: '2018-12-07 02:10:00',
-    y: 12
-}, {
-    x: '2018-12-08 00:00:00',
-    y: 1
-}, {
-    x: '2018-12-09 00:00:00',
-    y: 7
-},{
-    x: '2018-12-09 12:00:00',
-    y: 10
-},{
-    x: '2018-12-09 20:00:00',
-    y: 15
-},{
-    x: '2018-12-10 19:00:00',
-    y: 9
-}];
