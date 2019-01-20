358779c66cc01cccb92ae82aa6996bde0d571feb
Kristian <kristian.morin@gmail.com>
2019-01-20 13:19:27 +0000

    Added responsive sidebar

--- public/js/main.js
+++ public/js/main.js
@@ -278,16 +278,24 @@ function drawTargetTemp() {
   myLineChart.update();
 }
 
-$(function() {
-  $('input[name="daterange"]').daterangepicker({
-    opens: 'left',
-    drops: 'down',
-    endDate: moment().startOf('hour'),
-    startDate: moment().startOf('hour').subtract(5, 'day'),
-  }, function(start, end, label) {
-    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    let from  = start.format('YYYY-MM-DD');
-    let to = end.format('YYYY-MM-DD')
-    plotDataBetween(sensor, from, to);
+$(document).ready(function () {
+
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
   });
+
 });
+// Date picker
+// $(function() {
+//   $('input[name="daterange"]').daterangepicker({
+//     opens: 'left',
+//     drops: 'down',
+//     endDate: moment().startOf('hour'),
+//     startDate: moment().startOf('hour').subtract(5, 'day'),
+//   }, function(start, end, label) {
+//     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+//     let from  = start.format('YYYY-MM-DD');
+//     let to = end.format('YYYY-MM-DD')
+//     plotDataBetween(sensor, from, to);
+//   });
+// });
